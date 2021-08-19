7232382a53bfcb0918d62c39c0b347d9d1492011
Ramón Huidobro <hola@ramonh.dev>
2021-08-19 17:06:41 +0000

    feat(refactor): Extract ShipmentCategoryIcon component, adjust spacing and order of icons

--- src/templates/RegionPage.tsx
+++ src/templates/RegionPage.tsx
@@ -1,6 +1,7 @@
 import { graphql } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import { FunctionComponent } from 'react'
+import ShipmentCategoryIcon from '../components/icons/ShipmentCategoryIcon'
 import SimpleLayout from '../layouts/Simple'
 import {
   AllContentfulDataImpactShipment,
@@ -12,150 +13,6 @@ interface Props {
     contentfulDataGeoRegion: ContentfulDataGeoRegion
     toRegions: AllContentfulDataImpactShipment
     fromRegions: AllContentfulDataImpactShipment
-  }
-}
-
-const categorizeShipment = function (
-  shipment: any,
-  region: ContentfulDataGeoRegion,
-) {
-  const { fromSubregions, toSubregions } = shipment
-
-  // fromSubregions might be null, guard against that
-  const isOutgoing: Boolean = (fromSubregions || []).find((subregion: any) => {
-    return subregion.region.contentful_id === region.contentful_id
-  })
-
-  // toSubregions might be null, guard against that
-  const isIncoming: Boolean = (toSubregions || []).find((subregion: any) => {
-    return subregion.region.contentful_id === region.contentful_id
-  })
-
-  if (isOutgoing && isIncoming) {
-    return (
-      <div aria-label="Internal Transfer">
-        <svg
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          className="inline-block h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M17 8l4 4m0 0l-4 4m4-4H3"
-          />
-        </svg>
-        <svg
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          className="inline-block h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0"
-          />
-        </svg>
-        <svg
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          className="inline-block h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M7 16l-4-4m0 0l4-4m-4 4h18"
-          />
-        </svg>
-      </div>
-    )
-  } else if (isOutgoing) {
-    return (
-      <div aria-label="Outgoing">
-        <svg
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          className="inline-block h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0"
-          />
-        </svg>
-        <svg
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          className="inline-block h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M17 8l4 4m0 0l-4 4m4-4H3"
-          />
-        </svg>
-      </div>
-    )
-  } else if (isIncoming) {
-    return (
-      <div aria-label="Incoming">
-        <svg
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          className="inline-block h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0"
-          />
-        </svg>
-        <svg
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          className="inline-block h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M7 16l-4-4m0 0l4-4m-4 4h18"
-          />
-        </svg>
-      </div>
-    )
-  } else {
-    console.log('Invalid GraphQL Data')
   }
 }
 
@@ -177,12 +34,6 @@ const RegionPageTemplate: FunctionComponent<Props> = ({ data }) => {
         return item.contentful_id === shipment.contentful_id
       })
       if (!hasShipment) {
-        // Goal: categorize each shipment
-        //   - incoming
-        //   - outgoing
-        //   - local-transfers
-        shipment.category = categorizeShipment(shipment, region)
-
         accumulator.push(shipment)
       }
       return accumulator
@@ -221,7 +72,9 @@ const RegionPageTemplate: FunctionComponent<Props> = ({ data }) => {
           return (
             <tr>
               <td>{shipment.name}</td>
-              <td>{shipment.category}</td>
+              <td>
+                <ShipmentCategoryIcon shipment={shipment} region={region} />
+              </td>
               <td>{shipment.deliveredOn}</td>
               <td>{shipment.totalCommercialValue}</td>
               <td>{shipment.totalWeight}</td>
