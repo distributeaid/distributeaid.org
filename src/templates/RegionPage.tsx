decaec03b096b4a411c8b47a59041f28965f601e
Taylor Fairbank <taylor@distributeaid.org>
2021-08-26 17:00:51 +0200

    WIP: GLMP Hack Day 2 (#21)
    * feat(dev): gitpod configuration setup
    
    * feat(dev): Setup contributors file w/ a contributor agreement.
    
    * fix(dev): Fix gitpod start up task commands.
    
    * feat: Added Image to where we work region page
    Edit contributors
    
    * feat: added thumbnails in region list
    
    * feat(test): Add jest & react testing library.
    
    * feat: Display unformed to and from shipments
    
    * feat: Replace paragraph with table
    
    * chore: Gherkin test scenario comment
    
    * feat: filtering out duplicate shipments on the regions page
    
    * chore(deps): Upgrade dependencies and remove unnecessary gatsby-image.
    
    * feat: Add additional shipment data to the regions page table.  Remove debugging statements.
    
    * feat: Categorize shipments and display categories on the regions page.
    
    * feat: Add SVG icons for categories with added accessibility
    
    * feat(refactor): Extract ShipmentCategoryIcon component, adjust spacing and order of icons
    
    Co-authored-by: Ramón Huidobro <hola@ramonh.dev>

--- src/templates/RegionPage.tsx
+++ src/templates/RegionPage.tsx
@@ -1,11 +1,18 @@
 import { graphql } from 'gatsby'
+import { GatsbyImage } from 'gatsby-plugin-image'
 import { FunctionComponent } from 'react'
+import ShipmentCategoryIcon from '../components/icons/ShipmentCategoryIcon'
 import SimpleLayout from '../layouts/Simple'
-import { ContentfulDataGeoRegion } from '../types/gatsby-graphql-types.gen'
+import {
+  AllContentfulDataImpactShipment,
+  ContentfulDataGeoRegion,
+} from '../types/gatsby-graphql-types.gen'
 
 interface Props {
   data: {
     contentfulDataGeoRegion: ContentfulDataGeoRegion
+    toRegions: AllContentfulDataImpactShipment
+    fromRegions: AllContentfulDataImpactShipment
   }
 }
 
@@ -16,6 +23,23 @@ const RegionPageTemplate: FunctionComponent<Props> = ({ data }) => {
   }
 
   const region = data.contentfulDataGeoRegion
+  const toShipments = data.toRegions.nodes
+  const fromShipments = data.fromRegions.nodes
+  const allShipmentsWithDuplicates = toShipments.concat(fromShipments)
+
+  // remove duplicate shipments
+  const allShipments = allShipmentsWithDuplicates.reduce(
+    (accumulator: Array<any>, shipment: any) => {
+      const hasShipment = accumulator.find((item) => {
+        return item.contentful_id === shipment.contentful_id
+      })
+      if (!hasShipment) {
+        accumulator.push(shipment)
+      }
+      return accumulator
+    },
+    [],
+  )
 
   return (
     <SimpleLayout pageContext={pageContext}>
@@ -27,6 +51,40 @@ const RegionPageTemplate: FunctionComponent<Props> = ({ data }) => {
       {/* page body */}
       <p>Hello World</p>
 
+      <GatsbyImage
+        image={region.mapPhoto?.gatsbyImageData}
+        alt="Map of {region.name}"
+      />
+      <h1 className="text-xl">All Shipments</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Date Delivered</th>
+            <th>Total Com. Value</th>
+            <th>Total Weight (kg)</th>
+            <th>Total Distance (km)</th>
+            <th>Total CO2 (Tons)</th>
+          </tr>
+        </thead>
+        {allShipments.map((shipment) => {
+          return (
+            <tr>
+              <td>{shipment.name}</td>
+              <td>
+                <ShipmentCategoryIcon shipment={shipment} region={region} />
+              </td>
+              <td>{shipment.deliveredOn}</td>
+              <td>{shipment.totalCommercialValue}</td>
+              <td>{shipment.totalWeight}</td>
+              <td>{shipment.totalDistance}</td>
+              <td>{shipment.totalC02}</td>
+            </tr>
+          )
+        })}
+      </table>
+
       {/* page footer */}
       <footer>
         <p>Page Footer</p>
@@ -35,6 +93,18 @@ const RegionPageTemplate: FunctionComponent<Props> = ({ data }) => {
   )
 }
 
+/*
+Scenario Outline:
+Given shipment to and from "<region>"
+When we ask for the to shipments
+Then the destination is among "<destinations>"
+
+Examples:
+| region | destinations |
+| Greece | Lesvos,Chios,Greece |
+| France | Calais,Dunkirk,France |
+*/
+
 export default RegionPageTemplate
 
 export const regionQuery = graphql`
@@ -42,6 +112,89 @@ export const regionQuery = graphql`
     contentfulDataGeoRegion(contentful_id: { eq: $regionContentfulId }) {
       contentful_id
       name
+      mapPhoto {
+        gatsbyImageData
+      }
+    }
+    toRegions: allContentfulDataImpactShipment(
+      filter: {
+        toSubregions: {
+          elemMatch: { region: { contentful_id: { eq: $regionContentfulId } } }
+        }
+      }
+    ) {
+      nodes {
+        contentful_id
+        deliveredOn
+        name
+        slug
+        numPickups
+        numDropoffs
+        totalCommercialValue
+        totalDistance
+        totalWeight
+        totalC02
+        fromSubregions {
+          contentful_id
+          name
+          slug
+          region {
+            name
+            slug
+            contentful_id
+          }
+        }
+        toSubregions {
+          contentful_id
+          name
+          slug
+          region {
+            name
+            slug
+            contentful_id
+          }
+        }
+      }
+    }
+    fromRegions: allContentfulDataImpactShipment(
+      filter: {
+        fromSubregions: {
+          elemMatch: { region: { contentful_id: { eq: $regionContentfulId } } }
+        }
+      }
+    ) {
+      nodes {
+        contentful_id
+        deliveredOn
+        name
+        slug
+        numPickups
+        numDropoffs
+        totalCommercialValue
+        totalDistance
+        totalWeight
+        totalC02
+        fromSubregions {
+          contentful_id
+          name
+          slug
+          region {
+            name
+            slug
+            contentful_id
+          }
+        }
+        toSubregions {
+          contentful_id
+          name
+          slug
+          region {
+            name
+            slug
+            contentful_id
+          }
+        }
+      }
     }
   }
 `
