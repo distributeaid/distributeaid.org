8a79d0a5a5246109763f07f0a6b131cce4547d83
khendrikse <k.hendrikse@youngcapital.nl>
2021-10-19 19:56:47 +0200

    add tests for button

--- src/components/button/Button.test.js
+++ src/components/button/Button.test.js
@@ -3,9 +3,31 @@ import { render, fireEvent, screen } from '@testing-library/react'
 
 describe('Button', () => {
   it('renders the children', () => {
-    render(<Button>A button</Button>)
+    const { getByText } = render(<Button>A button</Button>)
 
-    const button = screen.getByText('A button')
+    const button = getByText('A button')
     expect(button).toBeTruthy()
+  })
+
+  it('sets aria-disabled as true if disabled is passed as a prop', () => {
+    const { getByText } = render(<Button disabled>A disabled button</Button>)
+
+    const button = getByText('A disabled button')
+
+    expect(button).toHaveAttribute('aria-disabled', 'true')
+    expect(button).toBeDisabled()
+  })
+
+  it('triggers the onClick if clicked', () => {
+    const mockOnClick = jest.fn()
+
+    const { getByText } = render(
+      <Button onClick={mockOnClick}>A clickable button</Button>,
+    )
+    expect(mockOnClick).not.toHaveBeenCalled()
+
+    fireEvent.click(getByText('A clickable button'))
+
+    expect(mockOnClick).toHaveBeenCalled()
   })
 })
