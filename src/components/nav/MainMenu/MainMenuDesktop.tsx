48f4b573f2ad3846d4904da0d068e30513e23056
Maxime Preaux (he/him) <deammer@users.noreply.github.com>
2021-11-22 23:16:47 -0800

    Build route pages (#104)
    * Build the route pages from the current site
    
    * Added the routes to the nav
    
    * Build the Lebanon route
    
    * Added background images
    
    * Added public documents
    
    * Removed unused CSS and improved the nav dropdown
    
    * Remove more CSS

--- src/components/nav/MainMenu/MainMenuDesktop.tsx
+++ src/components/nav/MainMenu/MainMenuDesktop.tsx
@@ -1,32 +1,57 @@
+import { useState, useRef, useEffect } from 'react'
 import { Link } from 'gatsby'
 import { FunctionComponent } from 'react'
 import { NavLinkItem } from './MainMenu'
 
 interface Props {
   navLinks: NavLinkItem[]
+  routeLinks?: NavLinkItem[]
 }
 
-const DesktopNavigation: FunctionComponent<Props> = ({ navLinks }) => {
+const DesktopNavigation: FunctionComponent<Props> = ({
+  navLinks,
+  routeLinks,
+}) => {
   return (
     <nav role="navigation" className="hidden md:block">
-      <ul className="pl-6 flex space-x-2 items-center">
+      <div className="pl-6 flex space-x-2 items-center">
         {navLinks.map((link) => (
-          <li key={link.path}>
-            <Link
-              to={link.path}
-              className="py-2 px-4 rounded hover:bg-navy-700 transition-colors text-white"
-            >
-              {link.title}
-            </Link>
-          </li>
+          <Link
+            key={link.path}
+            to={link.path}
+            className="py-2 px-4 rounded hover:bg-navy-700 transition-colors text-white"
+          >
+            {link.title}
+          </Link>
         ))}
+        {routeLinks && (
+          <nav className="nav-dropdown">
+            <a
+              href="#"
+              className="nav-dropdown-trigger py-2 px-4 text-white block rounded-t"
+            >
+              Routes
+            </a>
+            <ul className="nav-dropdown-content bg-navy-700 shadow-lg">
+              {routeLinks.map((route) => (
+                <Link
+                  to={route.path}
+                  key={route.path}
+                  className="py-2 px-4 rounded block hover:bg-navy-600 transition-colors text-white whitespace-nowrap"
+                >
+                  {route.title}
+                </Link>
+              ))}
+            </ul>
+          </nav>
+        )}
         <Link
           to="/donate"
           className="py-2 px-6 rounded bg-white transition-colors text-navy-700"
         >
           Donate
         </Link>
-      </ul>
+      </div>
     </nav>
   )
 }
