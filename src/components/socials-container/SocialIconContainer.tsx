ce6fc0b39724cc2fa99208cc791323dab549e759
Ramón Huidobro <hola@ramonh.dev>
2021-12-15 11:10:06 +0100

    Integrate social icon SVGs (#121)
    * feature: Expand YouTube embed width
    
    * feature: add icon SVGs, setup skeleton for icon component

--- src/components/socials-container/SocialIconContainer.tsx
+++ src/components/socials-container/SocialIconContainer.tsx
@@ -1,1 +1,10 @@
-;<div>Social Icons Go Here.</div>
+import React, { FC } from 'react'
+
+import facebookSrc from '../../images/social-icons/facebook.svg'
+import twitterSrc from '../../images/social-icons/twitter.svg'
+import linkedInSrc from '../../images/social-icons/linked-in.svg'
+import instagramSrc from '../../images/social-icons/instagram.svg'
+
+const SocialIconContainer: FC = () => <div>Social icons go here</div>
+
+export default SocialIconContainer
