241a0ce2e115853a8604bb2a93479314944624e8
Maxime Preaux (he/him) <deammer@users.noreply.github.com>
2022-01-17 12:03:26 -0800

    Fix the video size in browsers that don't support aspect-ratio (#142)
    * Fix the video size in browsers that don't support aspect-ratio
    
    * Hopefully fixed the feature detection
    
    * I'm an idiot

--- src/components/about-us/AboutHero.tsx
+++ src/components/about-us/AboutHero.tsx
@@ -2,17 +2,16 @@ import { FC } from 'react'
 
 const AboutHero: FC = () => (
   <div className="py-8 md:py-20 mx-auto" style={{ maxWidth: 800 }}>
-    <iframe
-      style={{
-        aspectRatio: '16/9',
-        width: '100%',
-      }}
-      src="https://www.youtube.com/embed/msizPweg3kE"
-      title="YouTube video player"
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-    />
+    <div className="video-container">
+      <iframe
+        className="video-iframe"
+        src="https://www.youtube.com/embed/msizPweg3kE"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </div>
   </div>
 )
 
