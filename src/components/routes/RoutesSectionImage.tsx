062d316eaee6dbc472de31334bab300af28b24e4
Rabea Gleissner <RabeaGleissner@users.noreply.github.com>
2022-01-25 11:43:57 +0800

    Mobile responsiveness for routes pages (#146)
    * Create new component for sections on routes page
    
    * Use new routes section component on routes page
    
    * Remove unused legacy CSS
    
    * Center background image properly and improve aria label
    
    * Use new routes section component in second routes page

---
+++ src/components/routes/RoutesSectionImage.tsx
@@ -0,0 +1,17 @@
+import { FunctionComponent, ReactNode } from 'react'
+
+interface Props {
+  image: string
+  ariaLabel: string
+}
+
+const RoutesSectionImage: FunctionComponent<Props> = ({ image, ariaLabel }) => (
+  <div
+    role="img"
+    aria-label={ariaLabel}
+    className="bg-center bg-no-repeat bg-cover bg-slate-200 w-full md:w-2/4 h-96 md:h-auto"
+    style={{ backgroundImage: `url(${image})` }}
+  ></div>
+)
+
+export default RoutesSectionImage
