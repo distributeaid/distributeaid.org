b828f7d5ef85f815b6aa1466183d1fe0b424b8a8
Taylor Fairbank <taylor@distributeaid.org>
2022-04-06 01:13:14 -0400

    refactor: Gatsby Templates => Gatsby Pages for the Forestry sourced Home page.

--- gatsby-node.js
+++ gatsby-node.js
@@ -52,36 +52,6 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return
   }
 
-  // Build the home page
-  const homePageQuery = await graphql(`
-    query HomePageQuery {
-      file(relativePath: { eq: "home.md" }) {
-        id
-        childMarkdownRemark {
-          frontmatter {
-            headline
-            missionStatement
-            howWeHelpBlock {
-              description
-              location
-              title
-              image
-              imageAlt
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  createPage({
-    path: '/',
-    component: path.resolve(`./src/templates/HomePage.tsx`),
-    context: {
-      pageFields: homePageQuery.data.file.childMarkdownRemark.frontmatter,
-    },
-  })
-
   /**
    * Build the routes! For each page in the content/routes directory, we create
    * a page using its path.
