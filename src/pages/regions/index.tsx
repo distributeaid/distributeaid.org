26deb2ecf744117b1b7e304e518017d19470dd6c
Taylor Fairbank <taylor@distributeaid.org>
2022-05-16 17:57:52 -0400

    feat: Convert all region / subregion queries to use the new DARegion & DASubregion graphql types.

--- src/pages/regions/index.tsx
+++ src/pages/regions/index.tsx
@@ -2,28 +2,12 @@ import { FC } from 'react'
 import SimpleLayout from '@layouts/Simple'
 import { graphql } from 'gatsby'
 import RegionCard from '@components/regions/RegionCard'
-import {
-  RegionSummary,
-  SubregionSummary,
-} from '@components/regions/RegionComponentTypes'
+import { Region } from '@components/regions/RegionComponentTypes'
 
 type Props = {
   data: {
     regions: {
-      nodes: [
-        {
-          fileAbsolutePath: string
-          frontmatter: RegionSummary
-        },
-      ]
-    }
-    subregions: {
-      nodes: [
-        {
-          fileAbsolutePath: string
-          frontmatter: SubregionSummary
-        },
-      ]
+      nodes: Region[]
     }
   }
 }
@@ -31,41 +15,14 @@ type Props = {
 const RegionsPage: FC<Props> = ({
   data: {
     regions: { nodes: regions },
-    subregions: { nodes: subregions },
   },
 }) => {
-  const subregionLookup = subregions.reduce(
-    (
-      lookup: Record<string, SubregionSummary>,
-      { fileAbsolutePath, frontmatter: subregion },
-    ) => {
-      const fileRelativePath =
-        'content/pages/regions/' +
-        fileAbsolutePath.split('content/pages/regions/')[1]
-      lookup[fileRelativePath] = subregion
-      return lookup
-    },
-    {},
-  )
-
-  const regionCards = regions.map(
-    ({ fileAbsolutePath, frontmatter: region }) => {
-      const subregions = region.subregions.reduce(
-        (collector: SubregionSummary[], fileRelativePath) => {
-          collector.push(subregionLookup[fileRelativePath])
-          return collector
-        },
-        [],
-      )
-
-      return <RegionCard region={region} subregions={subregions} />
-    },
-  )
-
   return (
     <SimpleLayout pageTitle="Regions">
       <section className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 px-4 lg:px-8 py-12 lg:py-24 max-w-7xl mx-auto">
-        {regionCards}
+        {regions.map((region) => (
+          <RegionCard region={region} />
+        ))}
       </section>
     </SimpleLayout>
   )
@@ -74,30 +31,15 @@ const RegionsPage: FC<Props> = ({
 export default RegionsPage
 
 export const pageQuery = graphql`
-  query RegionsPageQuery {
-    regions: allMarkdownRemark(
-      filter: {
-        fileAbsolutePath: { glob: "**/content/pages/regions/*/index.md" }
-      }
-    ) {
+  query RegionsQuery {
+    regions: allDaRegion {
       nodes {
-        fileAbsolutePath
-        frontmatter {
-          name
-          map
-          overview
-          subregions
+        name
+        map {
+          gatsbyImageData
         }
-      }
-    }
-    subregions: allMarkdownRemark(
-      filter: {
-        fileAbsolutePath: { glob: "**/content/pages/regions/*/!(index).md" }
-      }
-    ) {
-      nodes {
-        fileAbsolutePath
-        frontmatter {
+        overview
+        subregions {
           name
           population {
             count
