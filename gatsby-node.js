1e2de418f17f52b88e71c883fff4efdfaf93b92b
Taylor Fairbank <taylor@distributeaid.org>
2022-05-16 16:42:14 -0400

    feat: Create gatsby graphql resolvers for Subregion map & region fields.

--- gatsby-node.js
+++ gatsby-node.js
@@ -136,7 +136,7 @@ exports.createResolvers = ({ createResolvers, getNode }) => {
       subregions: {
         type: ['DASubregion'],
         resolve: async (source, args, context, info) => {
-          const { entries } = await context.nodeModel.findAll({
+          const { entries: subregions } = await context.nodeModel.findAll({
             query: {
               filter: {
                 fileRelativePath: { in: source.subregionFileRelativePaths },
@@ -144,13 +144,14 @@ exports.createResolvers = ({ createResolvers, getNode }) => {
             },
             type: 'DASubregion',
           })
-          return entries
+          return subregions
         },
       },
+
       map: {
         type: 'ImageSharp',
         resolve: async (source, args, context, info) => {
-          const entry = await context.nodeModel.findOne({
+          const file = await context.nodeModel.findOne({
             query: {
               filter: {
                 absolutePath: {
@@ -161,7 +162,48 @@ exports.createResolvers = ({ createResolvers, getNode }) => {
             type: 'File',
           })
 
-          return getNode(entry.children[0])
+          const imageSharp = file ? getNode(file.children[0]) : null
+
+          return imageSharp
+        },
+      },
+    },
+
+    DASubregion: {
+      region: {
+        type: 'DARegion',
+        resolve: async (source, args, context, info) => {
+          const region = await context.nodeModel.findOne({
+            query: {
+              filter: {
+                subregionFileRelativePaths: { eq: source.fileRelativePath },
+              },
+            },
+            type: 'DARegion',
+          })
+          return region
+        },
+      },
+
+      // NOTE: Same as resolver for DARegion.map above.
+      // TODO: Refactor into a graphql fragment or reusable function.
+      map: {
+        type: 'ImageSharp',
+        resolve: async (source, args, context, info) => {
+          const file = await context.nodeModel.findOne({
+            query: {
+              filter: {
+                absolutePath: {
+                  glob: `**/static${source.mapFileRelativePath}`,
+                },
+              },
+            },
+            type: 'File',
+          })
+
+          const imageSharp = file ? getNode(file.children[0]) : null
+
+          return imageSharp
         },
       },
     },
