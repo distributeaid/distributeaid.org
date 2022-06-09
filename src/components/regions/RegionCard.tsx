31bfcb4a36a96d9910d40f6e0812d3625d1b6fb9
Ramón Huidobro <hola@ramonh.dev>
2022-06-09 18:03:48 +0000

    feat: Create links from the regions index page to each region page.

--- src/components/regions/RegionCard.tsx
+++ src/components/regions/RegionCard.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from '@reach/router/node_modules/@types/react'
 import React, { FC } from 'react'
+import slugify from 'utils/slugify'
+import { getOxfordCommaSeparator } from 'utils/strings'
 import SmartLink from '@components/link/SmartLink'
 import MarkdownContent from '@components/markdown/MarkdownContent'
 import {
@@ -14,37 +16,48 @@ type Props = {
 }
 
 const RegionCard: FC<Props> = ({ region, subregions }) => {
-  const subregionNames = Object.entries(subregions)
-    .reduce((names: string, [relativeFilePath, subregion]) => {
-      return names + subregion.name + ' // '
-    }, '')
-    .slice(0, -4)
+  const regionSlug = slugify(region.name)
+  const regionHref = '/regions/' + regionSlug
+
+  const subregionLinks = Object.entries(subregions).map(
+    ([fileRelativePath, subregion], index, array) => {
+      const subregionSlug = slugify(subregion.name)
+      const subregionHref = regionHref + '#' + subregionSlug
+      const seperator = getOxfordCommaSeparator(index, array)
+      return (
+        <span>
+          {seperator}
+          <SmartLink className="link" href={subregionHref}>
+            {subregion.name}
+          </SmartLink>
+        </span>
+      )
+    },
+  )
 
   const populationCount = Object.entries(subregions)
-    .reduce((count: number, [relativeFilePath, subregion]) => {
+    .reduce((count: number, [fileRelativePath, subregion]) => {
       return count + subregion.population.count
     }, 0)
     .toLocaleString()
 
   return (
     <article className="border">
-      {region.map}
-      <div className="text-left">
+      <div class="m-3">
         <h2 className="text-xl font-semibold mb-1">{region.name}</h2>
-        <p className="text-gray-600 mb-6">{subregionNames}</p>
-        <p>
+        <p className="mb-3">{subregionLinks}</p>
+        <p className="mb-3">
           <strong>Population:</strong> {populationCount}
         </p>
-        {/*      <SmartLink
-          className="inline-block bg-navy-700 hover:bg-navy-800 rounded text-white px-6 py-3 font-medium tracking-wide"
-          href={ctaUrl}
+        <div className="mb-3 space-y-2 line-clamp-3">
+          <MarkdownContent content={region.overview} />
+        </div>
+        <SmartLink
+          className="mb-3 inline-block bg-navy-700 hover:bg-navy-800 rounded text-white px-6 py-3 font-medium tracking-wide"
+          href={regionHref}
         >
-          {ctaLabel}
+          View Region
         </SmartLink>
-  */}
-      </div>
-      <div className="m-4 mt-6 space-y-2 line-clamp-3">
-        <MarkdownContent content={region.overview} />
       </div>
     </article>
   )
