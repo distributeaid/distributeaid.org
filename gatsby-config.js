d086bdf648fa4712ac54d3a19a35b54aabfdbd15
Ramón Huidobro <hola@ramonh.dev>
2022-08-04 18:54:18 +0000

    feat: load JSON data into GraphQL, add vis test page

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,5 +81,12 @@ module.exports = {
         },
       },
     },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./content/data/`,
+      },
+    },
   ],
 }
