2b6fc8e3df7a5bda9e2cc471221e5e7ee41be8ef
teachjenntech <hello@jennjunod.com>
2022-08-25 16:53:14 +0000

    feat: New page template creation

---
+++ src/pages/page-template.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import SimpleLayout from '@layouts/Simple'
+import { Link } from 'gatsby'
+
+const PageTemplate: FC = () => (
+  <SimpleLayout pageTitle="Page Template">
+    <h1>Hello beautiful human!</h1>
+  </SimpleLayout>
+)
+
+export default PageTemplate
