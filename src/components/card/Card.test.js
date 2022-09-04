b136e6e7134c30613b937302e977d514e8139162
Jason Fritsche <34847680+JasonFritsche@users.noreply.github.com>
2022-09-04 15:15:16 -0500

    Update src/components/card/Card.test.js
    Co-authored-by: Markus Tacker <m@coderbyheart.com>

--- src/components/card/Card.test.js
+++ src/components/card/Card.test.js
@@ -1,7 +1,7 @@
 import Card from './Card'
 import { render } from '@testing-library/react'
 
-describe('Button', () => {
+describe('<Card/>', () => {
   it('renders the children', () => {
     const { getByText } = render(<Card>Card children</Card>)
     expect(getByText('Card children')).toBeInTheDocument()
