82d83e18af665735618929125c52e61534ef5e36
Markus Tacker <m@coderbyheart.com>
2022-09-05 21:25:20 +0200

    ci: run unit tests in ci

--- jest.config.js
+++ jest.config.js
@@ -8,11 +8,15 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
   },
-  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
+  testPathIgnorePatterns: [
+    `node_modules`,
+    `\\.cache`,
+    `<rootDir>.*/public`,
+    `<rootDir>.*/e2e-tests`,
+  ],
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
   globals: {
     __PATH_PREFIX__: ``,
   },
-  testURL: `http://localhost`,
   setupFiles: [`<rootDir>/loadershim.js`],
 }
