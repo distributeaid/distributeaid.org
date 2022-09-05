78fe1b38e5a4ca90046a8130b698d8bb5e736b3b
Jason Fritsche <fritsche.jason@gmail.com>
2022-09-05 10:33:09 -0500

    fix: fix failing unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: `jsdom`,
+  testMatch: ['**/?(*.)+(test).js'],
   setupFilesAfterEnv: ['<rootDir>/setup-test-env.js'],
   transform: {
     '^.+\\.[jt]sx?$': '<rootDir>/jest-preprocess.js',
@@ -7,6 +8,7 @@ module.exports = {
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+    '^@components/(.*)$': ['<rootDir>/src/components/$1'],
   },
   testPathIgnorePatterns: [
     `node_modules`,
