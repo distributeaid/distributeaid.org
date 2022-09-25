a73fc6f0ace94bce6fb8ccff814bc8098290dd18
KeSha Smith <103616990+KeShaKreates@users.noreply.github.com>
2022-09-25 10:44:22 -0700

    feat : team page created

---
+++ src/pages/team.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import SimpleLayout from '@layouts/Simple'
+
+const TeamPage: FC = () => {
+  return (
+    <SimpleLayout pageTitle="Team">
+      <h1 className="text-2xl">Meet the Team</h1>
+    </SimpleLayout>
+  )
+}
+
+export default TeamPage
