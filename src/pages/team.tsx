5618815c1b1d922f5e23e47b6eb3f0d65db5da9d
KeSha Smith <103616990+KeShaKreates@users.noreply.github.com>
2022-09-25 11:37:35 -0700

    feat: member data added to team page

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,12 +1,46 @@
 import { FC } from 'react'
 import SimpleLayout from '@layouts/Simple'
+import { graphql } from 'gatsby'
 
-const TeamPage: FC = () => {
+type Props = {
+  data: {
+    allDaTeamMember: {
+      nodes: any[]
+    }
+  }
+}
+const TeamPage: FC<Props> = ({ data }) => {
+  const members = data.allDaTeamMember.nodes
   return (
     <SimpleLayout pageTitle="Team">
       <h1 className="text-2xl">Meet the Team</h1>
+      <ul>
+        {members.map((member: any) => {
+          return (
+            <li>
+              <span>{member.name}</span>
+              <span>({member.pronouns})</span>
+            </li>
+          )
+        })}
+      </ul>
     </SimpleLayout>
   )
 }
 
 export default TeamPage
+
+export const pageQuery = graphql`
+  query TeamQuery {
+    allDaTeamMember {
+      nodes {
+        bio
+        name
+        pronouns
+        profilePhoto {
+          gatsbyImageData
+        }
+      }
+    }
+  }
+`
