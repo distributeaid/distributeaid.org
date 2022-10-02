15c77de05414172f44eb46d04674320f01d67859
Dorian Edwards <de.pharmd2018@gmail.com>
2022-10-02 16:42:58 -0400

    remove darkMode option to remove warning, closes #715

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   // Remove unused classes in prod to decrease the size of the CSS bundle
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
