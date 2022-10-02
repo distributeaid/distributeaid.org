de9fe87ef5c663731d9470aa87c4f7ecafa8ffc9
Taylor Fairbank <jtfairbank@gmail.com>
2022-10-02 16:17:50 -0500

    feat: Load data from multiple needs assessment surveys.

--- gatsby/source-nodes.js
+++ gatsby/source-nodes.js
@@ -12,54 +12,72 @@ module.exports = {
     reporter,
     createNodeId,
   }) => {
-    const survey = {
-      id: '01G4X7XTMCD6MFHDT5KS7Z27GF',
-      year: '2022',
-    }
-    const url = `https://storage.needs-assessment.distributeaid.dev/form/${survey.id}/summary?groupBy=timeOfYear.quarter,basicInfo.region`
-    const result = await fetch(url)
-    if (result.status !== 200) {
-      reporter.panic(
-        `Build failed`,
-        new Error(`Could not source needs assessment data: ${url}`),
-      )
-    }
-    const resultData = await result.json()
+    // NOTE: previous surveys were conducted with Google Forms, then Qualtrics.
+    //       need to load the data manually
+    const surveys = [
+      {
+        // Q2
+        id: '01FXN0BYG1X8509Y8DTDCB8WKB',
+        year: '2022',
+      },
+      {
+        // Q3
+        id: '01G4X7XTMCD6MFHDT5KS7Z27GF',
+        year: '2022',
+      },
+      {
+        // Q4
+        id: '01GCW8TQFCE92TR1NXH8NEET7F',
+        year: '2022',
+      },
+    ]
 
-    Object.entries(resultData.summary).forEach(([quarter, places]) => {
-      Object.entries(places).forEach(([place, pages]) => {
-        Object.entries(pages).forEach(([page, questions]) => {
-          if (categoryMap.hasOwnProperty(page)) {
-            Object.entries(questions).forEach(([question, units]) => {
-              const unitProp = Object.keys(units)[0]
-              const product = productMapper(page, question, unitProp)
-              const need = units[unitProp]
+    surveys.forEach(async (survey) => {
+      const url = `https://storage.needs-assessment.distributeaid.dev/form/${survey.id}/summary?groupBy=timeOfYear.quarter,basicInfo.region`
+      const result = await fetch(url)
+      if (result.status !== 200) {
+        reporter.panic(
+          `Build failed`,
+          new Error(`Could not source needs assessment data: ${url}`),
+        )
+      }
+      const resultData = await result.json()
 
-              createNode({
-                // Node Data
-                survey: {
-                  ...survey,
-                  quarter,
-                  place,
-                },
-                product,
-                need,
+      Object.entries(resultData.summary).forEach(([quarter, places]) => {
+        Object.entries(places).forEach(([place, pages]) => {
+          Object.entries(pages).forEach(([page, questions]) => {
+            if (categoryMap.hasOwnProperty(page)) {
+              Object.entries(questions).forEach(([question, units]) => {
+                const unitProp = Object.keys(units)[0]
+                const product = productMapper(page, question, unitProp)
+                const need = units[unitProp]
 
-                // Gatsby Fields
-                id: createNodeId(
-                  `DA Need - ${survey.id} ${quarter} ${place} ${JSON.stringify(
-                    product,
-                  )}`,
-                ),
-                parent: null,
-                children: [],
-                internal: {
-                  type: `DANeed`,
-                  contentDigest: createContentDigest(resultData),
-                },
+                createNode({
+                  // Node Data
+                  survey: {
+                    ...survey,
+                    quarter,
+                    place,
+                  },
+                  product,
+                  need,
+
+                  // Gatsby Fields
+                  id: createNodeId(
+                    `DA Need - ${
+                      survey.id
+                    } ${quarter} ${place} ${JSON.stringify(product)}`,
+                  ),
+                  parent: null,
+                  children: [],
+                  internal: {
+                    type: `DANeed`,
+                    contentDigest: createContentDigest(resultData),
+                  },
+                })
               })
-            })
-          }
+            }
+          })
         })
       })
     })
