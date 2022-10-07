7f30fb7810ef1eb7a3086c90b47c410f29795460
Dorian Edwards <de.pharmd2018@gmail.com>
2022-10-07 12:52:36 -0400

    add PageData component for use in page Head

---
+++ src/components/PageData.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import Favicon from './Favicon'
+
+const PageData: FC = () => (
+  <>
+    <meta
+      name="description"
+      content="Humanitarian aid delivery reimagined. By supporting a huge network of grassroots organisations, we ensure that donations get to where they are needed most."
+    />
+    <Favicon />
+  </>
+)
+
+export default PageData
