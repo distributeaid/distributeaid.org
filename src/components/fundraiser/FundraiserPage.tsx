e536efcb581826471c16afa6688ce93fec86a6c5
Markus Tacker <m@coderbyheart.com>
2022-10-18 23:56:14 +0200

    feat: update breakout page

--- src/components/fundraiser/FundraiserPage.tsx
+++ src/components/fundraiser/FundraiserPage.tsx
@@ -6,7 +6,7 @@ import { PageHeader } from '@components/PageHeader'
 import SimpleLayout from '@layouts/Simple'
 import { WaysToDonate } from '@pages/donate'
 import { GatsbyImage } from 'gatsby-plugin-image'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import '../../stylesheets/donate.css'
 
 export function Head({ pageContext: fundraiser }: { pageContext: Fundraiser }) {
@@ -18,44 +18,60 @@ export function Head({ pageContext: fundraiser }: { pageContext: Fundraiser }) {
   )
 }
 
-const FundraiserPhoto: FC<{ photo: Photo }> = ({ photo }) => {
-  return (
-    <div className="photo">
-      <GatsbyImage alt={photo.alt} image={photo.gatsbyImageData} />
-      <span>{photo.alt}</span>
-    </div>
-  )
-}
-
 export const FundraiserPage: FC<{ pageContext: Fundraiser }> = ({
   pageContext: fundraiser,
 }) => {
   return (
     <SimpleLayout
-      className={'donate fundraiser'}
+      className={'donate breakout'}
       footer={<Footer showDonateButton={false} />}
     >
-      <div className="bg" />
-      <header>
-        <h1>{fundraiser.title}</h1>
-      </header>
-      <FundraiserProgress fundraiser={fundraiser} title={'Campaign progress'} />
-      <article className="fundraiser">
-        <main>
-          <div className="prose">
+      <article>
+        <section>
+          <header>
             <h1>{fundraiser.title}</h1>
+          </header>
+          <div className="proseWrapper">
+            <MarkdownContent content={fundraiser.body} />
           </div>
-          <MarkdownContent content={fundraiser.body} />
-        </main>
-        <aside className="gallery">
-          {fundraiser.gallery.map((photo) => (
-            <FundraiserPhoto photo={photo} key={photo.url} />
-          ))}
+        </section>
+        <aside>
+          <div className="gallery">
+            <Gallery photos={fundraiser.gallery} />
+          </div>
+          <FundraiserProgress
+            fundraiser={fundraiser}
+            title={'Campaign progress'}
+          />
+          <div className="ways-to-donate">
+            <WaysToDonate />
+          </div>
         </aside>
       </article>
-      <section className="ways-to-donate">
-        <WaysToDonate />
-      </section>
     </SimpleLayout>
   )
+}
+
+const Gallery: FC<{ photos: Photo[] }> = ({ photos }) => {
+  const [current, setCurrent] = useState<string | undefined>(photos[0]?.url)
+
+  useEffect(() => {
+    if (photos.length === 0) return
+    if (current === undefined) return
+    const i = setInterval(() => {
+      const currentPhoto = photos.find(({ url }) => url === current)
+      if (currentPhoto === undefined) return
+      const index = photos.indexOf(currentPhoto)
+      setCurrent((photos[index + 1] ?? photos[0])?.url)
+    }, 5000)
+    return () => {
+      clearInterval(i)
+    }
+  }, [photos, current])
+
+  const photo = photos.find(({ url }) => url === current)
+
+  if (photo === undefined) return null
+
+  return <GatsbyImage alt={photo.alt} image={photo.gatsbyImageData} />
 }
