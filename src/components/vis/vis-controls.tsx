f570a63805c2c4fa945333ddfc8d8b871314fc53
Taylor Fairbank <jtfairbank@gmail.com>
2022-10-22 02:02:44 -0400

    refactor: move vis controls into their own module, style them

---
+++ src/components/vis/vis-controls.tsx
@@ -0,0 +1,88 @@
+import { FC, PropsWithChildren } from 'react'
+import Select, { Props as SelectProps } from 'react-select'
+
+type ControlSectionProps = {
+  label?: string
+  margin?: {
+    left?: number
+    right?: number
+  }
+}
+
+export const ControlSection: FC<PropsWithChildren<ControlSectionProps>> = ({
+  label,
+  margin,
+  children,
+}) => {
+  return (
+    <div
+      className="flex items-center gap-2"
+      style={{
+        marginLeft: !label ? '${margin.left}px' : ``,
+        marginRight: margin?.right ? `${margin.right}px` : '',
+      }}
+    >
+      {label && (
+        <h2
+          className="shrink-0 text-right m-0"
+          // leave space for the border & padding so it lines up with the chart
+          style={{
+            width: margin?.left ? `${margin.left - 10}px` : '',
+          }}
+        >
+          {label}
+        </h2>
+      )}
+
+      <div className="flex flex-wrap gap-5 border-l-2 py-5">
+        {children && <>{children}</>}
+      </div>
+    </div>
+  )
+}
+
+type SelectOption = {
+  value: string
+  label: string
+}
+
+type SelectControlProps = {
+  label: string
+  values: string[]
+  defaultValue?: string | undefined
+  setValue: React.Dispatch<React.SetStateAction<string | null>>
+} & SelectProps
+
+export const SelectControl: FC<SelectControlProps> = ({
+  label,
+  values,
+  defaultValue,
+  setValue,
+  ...props
+}) => {
+  const options = values.map((value) => {
+    return {
+      value: value,
+      label: value,
+    }
+  })
+  const defaultOption = defaultValue
+    ? { value: defaultValue, label: defaultValue }
+    : null
+
+  return (
+    <div className="flex items-center">
+      <label className="w-20 mr-2 text-right">{label}:</label>
+      <Select
+        className="w-64"
+        options={options}
+        defaultValue={defaultOption}
+        onChange={(untypedOption: any, actionMeta) => {
+          const option = untypedOption as SelectOption
+          setValue(option?.value || null)
+        }}
+        {...props}
+      />
+    </div>
+  )
+}
