2ee8400a1515e387d686563551681d501223aa48
Frédéric Bonnet <fredericbonnet@free.fr>
2022-10-23 11:37:08 +0000

    Add MainMenu stories and viewport addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-viewport',
   ],
   framework: '@storybook/react',
   core: {
