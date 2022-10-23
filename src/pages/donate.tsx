5d157a671deeba32a5b4209bc9df58e1bc9b244d
Markus Tacker <m@coderbyheart.com>
2022-10-23 14:16:06 +0200

    fix: remove Patreon
    See https://github.com/distributeaid/distributeaid.org/issues/786#issuecomment-1284338989

--- src/pages/donate.tsx
+++ src/pages/donate.tsx
@@ -4,7 +4,6 @@ import Footer from '@components/Footer'
 import ExternalLink from '@components/link/ExternalLink'
 import { PageHeader } from '@components/PageHeader'
 import SimpleLayout from '@layouts/Simple'
-import { StaticImage } from 'gatsby-plugin-image'
 import { FC, useState } from 'react'
 
 import directTransferImage from '../images/direct_transfer.svg'
@@ -27,33 +26,6 @@ const Donate: FC = () => {
         </h1>
 
         <div className="lg:flex max-w-5xl mx-auto">
-          <div className={cardClasses} data-test="patreon">
-            <div className="text-center">
-              <StaticImage
-                placeholder="tracedSVG"
-                src="../images/patreon_logo.png"
-                width={100}
-                height={100}
-                alt=""
-              />
-            </div>
-            <ExternalLink
-              className="link py-2"
-              href="https://www.patreon.com/distributeaid"
-            >
-              Patreon
-            </ExternalLink>
-            <p className="mt-4 text-gray-700">
-              Become a monthly supporter of Distribute Aid{' '}
-              <ExternalLink
-                href="https://www.patreon.com/distributeaid"
-                className="link"
-              >
-                through our Patreon
-              </ExternalLink>{' '}
-              account and get access to exclusive updates about our work.
-            </p>
-          </div>
           <div className={cardClasses} data-test="opencollective">
             <img
               src={openCollectiveImage}
