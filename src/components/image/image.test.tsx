2312981f42ed8d0b6a9e1fa3e07c03d57dcae560
Federico Chaves <federicochavesf@gmail.com>
2022-10-28 14:20:22 +0000

    started working in onMouseEnter

--- src/components/image/image.test.tsx
+++ src/components/image/image.test.tsx
@@ -1,10 +1,10 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import Image from './image'
 
 //capitalize image
 
 describe('image', () => {
-  it('renders the image', () => {
+  it('renders texts', () => {
     const { getByText } = render(
       <Image
         image={''}
@@ -18,8 +18,34 @@ describe('image', () => {
         alignment={'center'}
       />,
     )
-
+    const attr = getByText('DistributeAid')
     const paragraph = getByText('Our board members')
+    
     expect(paragraph).toBeTruthy()
+    expect(attr).toBe('DistributeAid')
+
   })
+
+  it('Toggle hidden when Mouse is Enter', () => {
+    const hovOnMouseEnter = jest.fn()
+    const { getByText } = render(
+      <Image
+        image={''}
+        width={600}
+        height={300}
+        altText={
+          'Our board members from left to right: Rudayna Abdo, Sara Lönegård, and Stephanie Fairbank'
+        }
+        caption={'Our board members'}
+        attribution={'DistributeAid'}
+        alignment={'center'}
+      />,
+    )
+    expect(hovOnMouseEnter).not.toHaveBeenCalled()
+
+    fireEvent.onMouseEnter(getByText('A clickable button'))
+
+    expect(hovOnMouseEnter).toHaveBeenCalled()
+  })
+
 })
