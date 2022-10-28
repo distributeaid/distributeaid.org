9e2abe24575bed466de43e784f6b27d904a04c7c
Rabea Gleissner <rabeagleissner@gmail.com>
2022-10-28 17:27:05 +0300

    Create StagingHubs component

--- src/pages/routes/{DARoute.slug}.tsx
+++ src/pages/routes/{DARoute.slug}.tsx
@@ -1,5 +1,4 @@
 import { graphql } from 'gatsby'
-import { StaticImage } from 'gatsby-plugin-image'
 import { FC } from 'react'
 
 import { PageHeader } from '@components/PageHeader'
@@ -14,6 +13,7 @@ import Frontline from '@components/routes/Frontline'
 import PhotoCredit from '@components/routes/PhotoCredit'
 import Reservations from '@components/routes/Reservations'
 import SectionTitle from '@components/routes/SectionTitle'
+import StagingHubs from '@components/routes/StagingHubs'
 import palletIcon from '../../images/regular-routes/icons/noun_Pallet_3307940.svg'
 import halfPalletIcon from '../../images/regular-routes/icons/noun_Pallet_3364535.svg'
 import sackIcon from '../../images/regular-routes/icons/openmoji_bag.svg'
@@ -64,68 +64,7 @@ const Routes: FC<TemplateProps> = ({ data: { route } }) => (
       aidRequestFormUrl={route.aidRequestFormUrl}
     />
     <Frontline images={route.images} frontlineGroups={route.frontlineGroups} />
-
-    <TextWithVisual
-      id="uk-staging-hubs"
-      positionOfVisual="left"
-      visual={
-        <iframe
-          className="w-full md:w-2/4 h-96 md:h-auto"
-          src={route.mapUrl}
-          width="100%"
-          height="100%"
-          title="routeMap"
-        />
-      }
-    >
-      <SectionTitle title="UK Staging Hubs" />
-      <div className="section__body">
-        <p className="mb-4">
-          The most <strong>cost efficient and Brexit / pandemic-proof</strong>{' '}
-          way to send aid from the UK is by shipping palletised aid on
-          articulated lorries that are loaded by a forklift.{' '}
-          <strong>That's where our UK Staging Hubs come in!</strong> They have
-          the necessary infrastructure and experience working with us to ensure
-          each shipment is fully optimized, which everybody benefits from. Once
-          it's in a Staging Hub, your aid will be palletised, stored, and loaded
-          by a forklift onto the next truck.
-        </p>
-
-        <div className="mb-12 flex">
-          <div className="tile-icon">
-            <StaticImage
-              src="../../images/regular-routes/pallet-aid-logo.256.png"
-              alt="Hub Logo: Pallet Aid (PA)"
-              height={80}
-              width={80}
-            />
-          </div>
-          <div className="tile-content ml-4">
-            <p className="text-lg font-medium mb-2">Coventry - South England</p>
-            <p className="text-sm leading-snug">
-              Both Community &amp; Commercial Aid
-            </p>
-            <p className="text-sm leading-snug">
-              pallets, loose boxes, bulk bags of tents &amp; sleeping bags, etc
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <footer>
-        <p className="text-sm italic text-center">
-          Questions? Comments? Contact us all at{' '}
-          <a
-            href="mailto:hubs@distributeaid.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hubs@distributeaid.org
-          </a>
-          .
-        </p>
-      </footer>
-    </TextWithVisual>
+    <StagingHubs mapUrl={route.mapUrl} />
 
     <TextWithVisual
       id="storage-and-shipping-charge"
