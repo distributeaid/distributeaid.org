80e2a84970bb214eaa39cf8ba171ef8de8617fc4
Jason Fritsche <fritsche.jason@gmail.com>
2022-12-11 11:58:18 -0600

    fix: restored testHost.ts to prod state

--- e2e-tests/lib/testHost.ts
+++ e2e-tests/lib/testHost.ts
@@ -1,6 +1,6 @@
 export const baseUrl = (): string => {
   const port = process.env.PORT ?? '8000'
   const hostname = process.env.HOSTNAME ?? 'localhost'
-  const baseUrl = `http://${'localhost'}:${port}`
+  const baseUrl = `http://${hostname}:${port}`
   return baseUrl
 }
