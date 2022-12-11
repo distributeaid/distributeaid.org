21f68921dc06d91b56593480d9a6751331048d23
Jason Fritsche <fritsche.jason@gmail.com>
2022-12-11 11:45:08 -0600

    feat: remove unused packages from project

--- e2e-tests/lib/testHost.ts
+++ e2e-tests/lib/testHost.ts
@@ -1,6 +1,6 @@
 export const baseUrl = (): string => {
   const port = process.env.PORT ?? '8000'
   const hostname = process.env.HOSTNAME ?? 'localhost'
-  const baseUrl = `http://${hostname}:${port}`
+  const baseUrl = `http://${'localhost'}:${port}`
   return baseUrl
 }
