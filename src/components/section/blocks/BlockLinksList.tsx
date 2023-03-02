455ee56188046e72a81c64fa48f2e836113c72f1
Ramón Huidobro <hola@ramonh.dev>
2023-03-02 18:07:13 +0000

    feat: Add generalised styling for individual blocks

--- src/components/section/blocks/BlockLinksList.tsx
+++ src/components/section/blocks/BlockLinksList.tsx
@@ -5,14 +5,15 @@ import { BlockTitle } from './BlockTitle'
 
 type BlockTextProps = {
   block: BlockLinksListType
+  className?: string | undefined
 }
 
-export const BlockLinksList: FC<BlockTextProps> = ({ block }) => {
+export const BlockLinksList: FC<BlockTextProps> = ({ block, className }) => {
   return (
     <>
       {/* TODO: replace w/ <BlockTitle> once that no longer needs a full Node */}
       {block.title && <BlockTitle block={{ text: block.title }} />}
-      <div className="border-l-2 pl-4 border-navy-400">
+      <div className={`${className} border-l-2 pl-4 border-navy-400`}>
         {/* NOTE: we're intentionally dropping the title so it doesn't get repeated */}
         <LinksList list={{ links: block.links }} />
       </div>
