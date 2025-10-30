4a6ac43f6e85448b0ce20595ddf7ed3f88c25748
jtfairbank <jtfairban@gmail.com>
2025-10-29 22:16:08 -0400

    feat: add donor recogition graphics

--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -5,6 +5,7 @@ import { PageHeader } from '@components/PageHeader'
 import SimpleLayout from '@layouts/Simple'
 import { graphql } from 'gatsby'
 import { FC } from 'react'
+import wingOfHonorSrc from '../../images/donate/wing-of-honor.png'
 import { Fundraiser } from '../../types/fundraiser.d'
 import { Direction } from '../../types/layout.d'
 
@@ -75,6 +76,38 @@ const DonatePage: FC<Props> = ({
           })}
         </article>
       )}
+
+      <section id="donor-wing-of-honor" className="bg-navy-50 px-4 pt-12">
+        <header className="prose max-w-screen-lg mx-auto">
+          <h1 className="text-center text-rosemary-800">Donor Wing of Honor</h1>
+          <h3>
+            These people have made extraordinary contributions to Distribute
+            Aid’s work around the world. Their support has enabled us to defend
+            human dignity and provide lifesaving aid to communities struck by
+            natural or manmade disasters. We will be forever grateful for their
+            generosity.
+          </h3>
+        </header>
+
+        <img
+          className="mx-auto"
+          src={wingOfHonorSrc}
+          alt="Distribute Aid Logo: A flock of doves stylized by stacking wings behind the main outline of a dove."
+        />
+      </section>
+
+      <footer className="bg-rosemary-50">
+        <div className="px-4 py-12">
+          <header className="prose max-w-none mx-auto mb-8">
+            <h1 className="text-center text-rosemary-800">
+              Ship aid to people in need.
+              <br />
+              Donate today:
+            </h1>
+          </header>
+          <WaysToDonate />
+        </div>
+      </footer>
     </SimpleLayout>
   )
 }
